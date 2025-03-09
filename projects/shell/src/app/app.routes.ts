0f01ec4c636b7397643a0e3c92126a17312b768b
shuangchen <guschenwpi@gmail.com>
2025-03-09 01:06:18 -0500

    Set up the mfe

---
+++ projects/shell/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import { HomeComponent } from './pages/home/home.component';
+// import { About }
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'todo',
+    loadComponent: () =>
+      loadRemoteModule('todo', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      loadRemoteModule('dashboard', './Component').then((m) => m.AppComponent),
+  },
+  // {
+  //   path: 'about',
+  //   loadChildren: () =>
+  //     import('about/Module').then((m) => m.AppModule),
+  // },
+  //wildcard route
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+];
